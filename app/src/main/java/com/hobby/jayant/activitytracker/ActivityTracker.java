2b3f77ebffc1eb48d20958be8a7841781913703a
Jayant Kumar Nayak <jayant.kumar.nayak@Sap.com>
2017-08-16 02:21:14 +0530

    Initial commit

---
+++ app/src/main/java/com/hobby/jayant/activitytracker/ActivityTracker.java
@@ -0,0 +1,18 @@
+package com.hobby.jayant.activitytracker;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by I329687 on 8/16/2017.
+ */
+
+public class ActivityTracker extends Application {
+    private static Context context;
+    public ActivityTracker(){}
+    public static Context getApplicationGlobalContext(){ return ActivityTracker.context;}
+    public void onCreate() {
+        super.onCreate();
+        ActivityTracker.context = getApplicationContext();
+    }
+}
---
+++ app/src/main/java/com/hobby/jayant/activitytracker/activities/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.hobby.jayant.activitytracker.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.hobby.jayant.activitytracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        int i;
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_login);
+    }
+}
